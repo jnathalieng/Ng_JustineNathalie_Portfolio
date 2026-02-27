26fc1870a63328e2213b9bec0d08514623e1bb29
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-02-27 14:25:40 -0500

    Update JS Folder with Scripts
    Added all the scripts from the previous code, just divided them to fit inside each modules.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+import { about } from "./modules/about.js";
+import { videoplayer } from "./modules/videoplayer.js";
+import { homepagesliders } from "./modules/homepagesliders.js";
+import { contact } from "./modules/contact.js";
+
+if (document.body.dataset.page === "about") {
+  about();
+}
+
+if (document.body.dataset.page === "videoplayer") {
+  videoplayer();
+}
+
+if (document.body.dataset.page === "homepagesliders") {
+  homepagesliders();
+}
+
+if (document.body.dataset.page === "contact") {
+  contact();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
