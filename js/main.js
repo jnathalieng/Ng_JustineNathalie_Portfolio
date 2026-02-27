23cc87ff9f698ae7546afc1f097588eb1e1e33ac
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-02-27 16:51:50 -0500

    Fix errors
    Fixing the errors in different folders.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { about } from "./modules/about.js";
 import { videoplayer } from "./modules/videoplayer.js";
 import { homepagesliders } from "./modules/homepagesliders.js";
 import { contact } from "./modules/contact.js";
-import { burgerMenu } from "./modules/burger.js";
+import { burgerMenu } from "./modules/burgermenu.js";
 import { gsapAnimations } from "./modules/gsap.js";
 
 burgerMenu();
