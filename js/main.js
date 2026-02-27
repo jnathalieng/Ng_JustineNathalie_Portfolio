577f6b8b7ac77a65232997a17ba88b1fcb2988c9
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-02-27 17:06:02 -0500

    Update main.js
    Change the burger to burgermenu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ import { about } from "./modules/about.js";
 import { videoplayer } from "./modules/videoplayer.js";
 import { homepagesliders } from "./modules/homepagesliders.js";
 import { contact } from "./modules/contact.js";
+<<<<<<< Updated upstream
+=======
+import { burgerMenu } from "./modules/burgermenu.js";
+import { gsapAnimations } from "./modules/gsap.js";
+
+burgerMenu();
+>>>>>>> Stashed changes
 
 if (document.body.dataset.page === "about") {
   about();
