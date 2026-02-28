6307e03536c984f2a3fda87ec053454d0999fe37
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-02-28 18:08:11 -0500

    Update login.php
    Updte correct codes for login.php

--- admin/login.php
+++ admin/login.php
@@ -42,6 +42,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link rel="shortcut icon" href="jn_favicon/favicon.ico" />
   <link rel="apple-touch-icon" sizes="180x180" href="jn_favicon/apple-touch-icon.png" />
   <link rel="manifest" href="jn_favicon/site.webmanifest" />
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
